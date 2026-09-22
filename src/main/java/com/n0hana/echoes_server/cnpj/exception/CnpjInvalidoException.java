a4bb32d8e3e5cead04752b46bf19370d0fdcbf27
Miguel Santana <miguelsantanadacosta@gmail.com>
2026-09-21 22:22:29 -0300

    feat/exception Cnpj

---
+++ src/main/java/com/n0hana/echoes_server/cnpj/exception/CnpjInvalidoException.java
@@ -0,0 +1,10 @@
+package com.n0hana.echoes_server.cnpj.exception;
+
+public class CnpjInvalidoException extendes RuntimeException {
+    public CnpjInvalidoException(String message) {
+        super(message);
+    }
+}
+
+
+
---
+++ src/main/java/com/n0hana/echoes_server/cnpj/exception/CnpjProviderIndisponivelException.java
@@ -0,0 +1,8 @@
+package com.n0hana.echoes_server.cnpj.exception;
+
+public class CnpjProviderIndisponivelException extends RuntimeException {
+    public CnpjProviderIndisponivelException(String message) {
+        super(message);
+    }
+}
+
