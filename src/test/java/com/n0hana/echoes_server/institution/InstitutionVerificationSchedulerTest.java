d0091592e5f4f1c1646f11faecf445f225d77ac1
Miguel Santana <miguelsantanadacosta@gmail.com>
2026-09-23 03:13:50 -0300

    renomeando arquivos

--- src/test/java/com/n0hana/echoes_server/institution/InstitutionVerificationSchedulerTest.java
+++ src/test/java/com/n0hana/echoes_server/institution/InstitutionVerificationSchedulerTest.java
@@ -1,3 +1,15 @@
+package com.n0hana.echoes_server.institution;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
 @ExtendWith(MockitoExtension.class)
 class InstitutionVerificationSchedulerTest {
     @Mock private InstitutionService institutionService;
